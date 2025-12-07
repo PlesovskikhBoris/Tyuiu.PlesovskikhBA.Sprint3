c6aa8674ea8de2a4a9e082de624fd40fe2c334e9
Administrator <Administrator@WIN-2ORNOHFEDP4>
2025-12-08 00:33:57 +0500

    Task7

---
+++ Tyuiu.PlesovskikhBA.Sprint3.Task7.V27/Program.cs
@@ -0,0 +1,55 @@
+﻿using Tyuiu.PlesovskikhBA.Sprint3.Task7.V27.Lib;
+namespace Tyuiu.PlesovskikhBA.Sprint3.Task7.V27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Плесовских Б. А. | СМАРТб-24-1";
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                  *");
+            Console.WriteLine("* Задание #7                                                                 *");
+            Console.WriteLine("* Вариант #27                                                                *");
+            Console.WriteLine("* Выполнил: Плесовских Борис Александрович | СМАРТб-24-1                     *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                   *");
+            Console.WriteLine("* Написать программу,которая выводит таблицу значения функций                *");
+            Console.WriteLine("* (произвести табулирование) f(x) на заданном диапазоне  [-5;5]              *");
+            Console.WriteLine("*                                                                            *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                           *");
+            Console.WriteLine("******************************************************************************");
+
+            int startValue = -5;
+            int stoptValue = 5;
+
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stoptValue);
+
+            int len = ds.GetMassFunction(startValue, stoptValue).Length;
+
+            double[] valueArray;
+            valueArray = new double[len];
+
+            valueArray = ds.GetMassFunction(startValue, stoptValue);
+
+
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                 *");
+            Console.WriteLine("******************************************************************************");
+
+            Console.WriteLine("+---------------------------------------+-------------------------------------");
+            Console.WriteLine("*|                  X                   |                  f(X)              |");
+            Console.WriteLine("+---------------------------------------+-------------------------------------");
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine("|{0,5:d}                                  | {1, 6:f2}                             |", startValue, valueArray[i]);
+                startValue++;
+            }
+            Console.WriteLine("+---------------------------------------+------------------------------------+");
+            Console.ReadKey();
+        }
+    }
+}
