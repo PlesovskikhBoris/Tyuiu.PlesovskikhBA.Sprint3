fdfaccffb7b4faef9030c4787d1b6b5039ad94ed
Administrator <Administrator@WIN-2ORNOHFEDP4>
2025-12-07 21:50:21 +0500

    Task1

---
+++ Tyuiu.PlesovskikhBA.Sprint3.Task1.V21/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.PlesovskikhBA.Sprint3.Task1.V21.Lib;
+namespace Tyuiu.PlesovskikhBA.Sprint3.Task1.V21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #3 | Выполнил: Плесовских Б. А. | СМАРТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Оператор цикла for                                                      *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #21                                                             *");
+            Console.WriteLine("* Выполнил: Плесовских Борис Александрович | СМАРТб-24-1                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*Написать программу используя цикл while, произведение ряда по формуле,   *");
+            Console.WriteLine("* при х=1                                                                 *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+            int value = 1;
+            int startValue = 1;
+            int stopValue = 12;
+
+            Console.WriteLine("Переменная Х = " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Произведение рада = " + ds.GetMultiplySeries(value, startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
