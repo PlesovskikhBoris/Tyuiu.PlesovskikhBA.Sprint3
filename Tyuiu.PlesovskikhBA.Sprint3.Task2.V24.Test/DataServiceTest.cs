5292d75951fe43f6d1378c5b84c9db6096f17aea
Administrator <Administrator@WIN-2ORNOHFEDP4>
2025-12-07 22:10:11 +0500

    Task2

---
+++ Tyuiu.PlesovskikhBA.Sprint3.Task2.V24.Test/DataServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.PlesovskikhBA.Sprint3.Task2.V24.Lib;
+namespace Tyuiu.PlesovskikhBA.Sprint3.Task2.V24.Test
+{
+    [TestClass]
+    public class Test1
+    {
+        [TestMethod]
+        public void TestGetMultiplySeries()
+        {
+            DataService ds = new DataService();
+            double a = 0.25;
+            int start = 1;
+            int stop = 8;
+
+            double expected = 1;
+            int k = start;
+            do
+            {
+                expected *= (Math.Pow(a, k) + 4) * Math.Cos(a);
+                k++;
+            }
+            while (k <= stop);
+
+            expected = Math.Round(expected, 3, MidpointRounding.AwayFromZero);
+            double actual = ds.GetMultiplySeries(a, start, stop);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Tyuiu.PlesovskikhBA.Sprint3.Task2.V24/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.PlesovskikhBA.Sprint3.Task2.V24.Lib;
+namespace Tyuiu.PlesovskikhBA.Sprint3.Task2.V24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #3 | Выполнил: Плесовских Б. А. | СМАРТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла do...while                                         *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #24                                                             *");
+            Console.WriteLine("* Выполнил: Плесовских Борис Александрович | СМАРТб-24-1                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Используя цикл do...while, вычислить произведение ряда:                 *");
+            Console.WriteLine("*        8                                                                *");
+            Console.WriteLine("*  p = ∏ (a^k + 4) * cos(a), при a = 0.25                                 *");
+            Console.WriteLine("*       k=1                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            double a = 0.25;
+            int startValue = 1;
+            int stopValue = 8;
+
+            DataService ds = new DataService();
+            double result = ds.GetMultiplySeries(a, startValue, stopValue);
+
+            Console.WriteLine($"Результат: p = {result:F3}");
+            Console.ReadKey();
+        }
+    }
+}
