9484062a8644ea570d7cc3a03801c2522a8a3ef5
Administrator <Administrator@WIN-2ORNOHFEDP4>
2025-12-07 23:46:45 +0500

    Task4

---
+++ Tyuiu.PlesovskikhBA.Sprint3.Task4.V12.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.PlesovskikhBA.Sprint3.Task4.V12.Lib;
+namespace Tyuiu.PlesovskikhBA.Sprint3.Task4.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+            double res = ds.Calculate(startValue, stopValue);
+            double next = 467.59;
+            Assert.AreEqual(res, next);
+        }
+    }
+}
---
+++ Tyuiu.PlesovskikhBA.Sprint3.Task4.V12/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.PlesovskikhBA.Sprint3.Task4.V12.Lib;
+namespace Tyuiu.PlesovskikhBA.Sprint3.Task4.V12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Плесовких Б. А. | СМАРТб-24-1";
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #3                                                              *");
+            Console.WriteLine("* Тема: Оператор цикла foreach                                           *");
+            Console.WriteLine("* Задание #4                                                             *");
+            Console.WriteLine("* Вариант #12                                                            *");
+            Console.WriteLine("* Выполнил: Плесовских Борис Александрович | СМАРТб-24-1                 *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5, вычислить значение    *");
+            Console.WriteLine("* y=x/(x + sin(x))+2.5 При х = 0 прервать цикл                           *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine("Сумма ряда = " + ds.Calculate(startValue, stopValue));
+
+            Console.ReadKey();
+
+        }
+    }
+}
